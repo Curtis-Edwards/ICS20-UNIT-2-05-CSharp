be51c84a3f80771099b483180ba16d4fd611e7bc
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2022-03-26 20:35:23 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,28 @@
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // This function accepts user input
+        int rate;
+        int hours;
+        double pay;
+        double govtaken;    
+
+        Console.WriteLine("Please enter your hourly wage and your hours worked");
+        Console.WriteLine("");
+
+        Console.Write("hourly wage: ");
+        rate = Convert.ToInt32(Console.ReadLine());
+        Console.Write("hours worked: ");
+        hours = Convert.ToInt32(Console.ReadLine());
+        pay = ( rate * hours ) * ( 1.00 - 0.18 );
+        govtaken = ( rate * hours ) * 0.18;
+        
+        Console.WriteLine("Your salary is: " + pay.ToString("0.00") + "$");
+        Console.WriteLine("The goverment will tkae: " + govtaken.ToString("0.00") + "$");
+                
+        Console.WriteLine("\nDone.");
+    }
+}
